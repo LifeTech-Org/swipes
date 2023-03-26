77721cd49baec981ff2b07bd7b097ec8e2be329f
LifeTech-Org <ayetigbosamuel01@gmail.com>
2023-03-26 17:06:53 +0100

    changed some content in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="h-12 flex items-center">
       <div className="text-sm text-blue-300 font-semibold">
-        Made with passion and sweat by{" "}
+        Made with love and knowledge by{" "}
       </div>{" "}
       <address className="text-blue-400 text-sm font-semibold ml-2 !underline underline-offset-8">
         <a href="https://wa.me/+2348149120163" title="Contact author">
